5968faaef7814454c18921dcd63b85b0f52944c0
iamquoz <46261678+iamquoz@users.noreply.github.com>
2026-03-06 12:46:36 +0400

    fix obtaining values for length filters

--- Calais/Core/ExpressionTreeBuilder.cs
+++ Calais/Core/ExpressionTreeBuilder.cs
@@ -515,7 +515,7 @@ namespace Calais.Core
             if (filter.Values == null || filter.Values.Count == 0)
                 return null;
 
-            var value = Convert.ToInt32(filter.Values[0]);
+            var value = ConvertValue(filter.Values[0]);
             var valueExpr = Expression.Constant(value);
 
             return op switch
